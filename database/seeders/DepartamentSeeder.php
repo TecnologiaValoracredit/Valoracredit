32eaaeb0fec0f955c69509601b49569fb39a2dfe
Ivan Rodriguez <auxtecnologia@valoracredit.mx>
2025-01-23 13:48:28 -0600

    Se agregaron los usuarios, roles y departamentos restantes

--- database/seeders/DepartamentSeeder.php
+++ database/seeders/DepartamentSeeder.php
@@ -15,14 +15,40 @@ class DepartamentSeeder extends Seeder
      */
     public function run()
     {
+        //1
         Departament::create([
             "name" => "Tecnología",
             "description" => "Departamento encargado de todo lo relacionado con tecnología, sistemas, redes, software, hardware y soporte técnico.",
         ]);
-
+        //2
         Departament::create([
             "name" => "Cartera",
             "description" => "Departamento encargado de todo lo relacionado con cartera, listados, llamadas de calidad",
+        ]);
+        //3
+        Departament::create([
+            "name" => "Contaduria",
+            "description" => "",
+        ]);
+        //4
+        Departament::create([
+            "name" => "Operaciones",
+            "description" => "",
+        ]);
+        //5
+        Departament::create([
+            "name" => "Tesoreria",
+            "description" => "",
+        ]);
+        //6
+        Departament::create([
+            "name" => "Recursos Humanos",
+            "description" => "",
+        ]);
+        //7
+        Departament::create([
+            "name" => "Marketing",
+            "description" => "",
         ]);
     }
 }
--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -15,45 +15,62 @@ class RoleSeeder extends Seeder
      */
     public function run()
     {
+        //1//
         Role::create([
             "name" => "Admin",
             "description" => "Admin"
         ]);
+        //2//
         Role::create([
             "name" => "Gerencia Cartera",
             "description" => "Encargados de cartera"
         ]);
+        //3//
         Role::create([
             "name" => "Tesorería",
             "description" => ""
         ]);
+        //4//
         Role::create([
             "name" => "Administración",
             "description" => ""
         ]);
+        //5//
         Role::create([
             "name" => "Contabilidad",
             "description" => ""
         ]);
+        //6//
         Role::create([
-            "name" => "Rrecursos Humanos",
+            "name" => "Recursos Humanos",
             "description" => ""
         ]);
+        //7//
         Role::create([
             "name" => "Cartera",
             "description" => ""
         ]);
+        //8//
         Role::create([
             "name" => "Marketing",
             "description" => ""
         ]);
+        //9//
         Role::create([
             "name" => "Tecnología",
             "description" => ""
         ]);
+        //10//
         Role::create([
             "name" => "Operaciones",
             "description" => ""
         ]);
+        //11//
+        Role::create([
+            "name" => "Cobranza",
+            "description" => ""
+        ]);
+
+        
     }
 }
