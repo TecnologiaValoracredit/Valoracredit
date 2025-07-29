b2776625230873c46bc6e056a7c704c32acfab45
AuxTecnologia <auxtecnologia@valoracredit.mx>
2025-07-29 17:48:40 -0600

    Columna Bank Id

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -42,18 +42,24 @@ class UserRequest extends FormRequest
 				Rule::unique('users')->ignore($userId), // Ignorar el correo electrónico del usuario actual
             ],
 			'bank_account' => [
+				'nullable',
 				'numeric',
-				'digits:16', 
+				'digits:18', 
 				//Si los primero 3 numeros de bank_account es igual a bank_code
 				 function ($attribute, $value, $fail) {
-					// Obtener los primeros 3 dígitos del número de cuenta
-					$accountPrefix = substr($value, 0, 3);
+					$bankId = $this->input('bank_id'); // obtener bank_id desde el request
 
-					// Buscar el banco con ese código
-					$bank = Bank::where('bank_code', $accountPrefix)->first();
+					$bank = Bank::find($bankId);
 
 					if (!$bank) {
-						$fail("No se encontró ningún banco con el código {$accountPrefix}.");
+						$fail('El banco no existe.');
+						return;
+					}
+
+					$accountPrefix = substr($value, 0, 3);
+
+					if ($accountPrefix !== $bank->bank_code) {
+						$fail("El número de cuenta no coincide con el código del banco ({$bank->bank_code}).");
 					}
 				}
 			],
